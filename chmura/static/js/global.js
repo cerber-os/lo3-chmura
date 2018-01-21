16771efa983fe9a2534e458838eabcdd7b6c3273
Michał Szopiński <jajnick@hotmail.com>
2018-01-21 17:34:41 +0100

    Prepare for timetable select addition

--- chmura/static/js/global.js
+++ chmura/static/js/global.js
@@ -6,4 +6,34 @@ function closeOverlay(div, event) {
 	document.getElementById("overlay").style.visibility = "hidden";
 }
 
+function setCookie(name, value, milliseconds) {
+	if (milliseconds) {
+		var date = new Date();
+		date.setTime(date.getTime() + milliseconds);
+		document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; expires=" + date.toUTCString() + "; path=/";
+	}
+	else
+		document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; path=/";
+}
+function getCookie(desiredName) {
+	var entries = document.cookie.split(/; ?/);
+	
+	for (var i in entries) {
+		var name = entries[i].substr(0, entries[i].indexOf("="));
+		name = decodeURIComponent(name);
+		
+		if (name != desiredName) continue;
+		
+		var value = entries[i].substr(entries[i].indexOf("=") + 1);
+		value = decodeURIComponent(value);
+		
+		return value;
+	}
+	
+	return null;
+}
+function eraseCookie(name) {
+	setCookie(name, "", -1);
+}
+
 window.onkeydown = function(event) { if (event.keyCode == 27) closeOverlay(); }
--- chmura/static/js/timetable.js
+++ chmura/static/js/timetable.js
@@ -1,4 +1,31 @@
-﻿function showDetails(a) {
+﻿function showTimetableSelect() {
+	document.getElementById("overlay").style.visibility = "visible";
+	document.getElementById("overlaytitle").innerText = "Wybierz plan";
+	
+	document.getElementById("overlaycontent").innerHTML = document.getElementById("timetableselectdialog").innerHTML;
+}
+function handleTimetableSelectUpdate(select) {
+	if (select.selectedIndex == select.children.length - 1) {
+		showTimetableSelect();
+		select.selectedIndex = select.getAttribute("data-previousselectedindex");
+		return;
+	}
+	
+	select.setAttribute("data-previousselectedindex", select.selectedIndex);
+}
+function handleTimetableTypeSelectUpdate(select) {
+	var showSecondSelect = select.selectedIndex == 1;
+	
+	select.parentElement.children[2].style.display = showSecondSelect ? "" : "none";
+	select.parentElement.children[3].style.display = showSecondSelect ? "" : "none";
+	select.parentElement.children[3].selectedIndex = 0;
+	showStudentsList(select.parentElement.children[3]);
+}
+function showStudentsList(classSelect) {
+	
+}
+
+function showDetails(a) {
 	document.getElementById("overlay").style.visibility = "visible";
 	document.getElementById("overlaytitle").innerText = "Szczegóły";
 	document.getElementById("overlaycontent").innerHTML = "";
