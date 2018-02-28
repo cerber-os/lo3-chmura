36a2f4735cacecebb33bad2cfd13e9258097ff35
cerber-os <contact@cerberos.pl>
2018-02-28 16:44:41 +0100

    Naprawiono błąd z poprzedniego commita

--- chmura/updateids.py
+++ chmura/updateids.py
@@ -73,8 +73,8 @@ def save_ids(ids):
         d = {}
         for uczen in ids['students']:
             nazwisko = re.search(r'(?<=[.[|])([A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ,]+)', uczen['lastname'])
-            nazwisko = nazwisko.replace(',', ' ')
             nazwisko = nazwisko.group(0) if nazwisko is not None else ""
+            nazwisko = nazwisko.replace(',', ' ')
             d.setdefault(klasy.get(uczen['classid'], '0'), []).append({'firstname': uczen['firstname'],
                                                                        'lastname': nazwisko,
                                                                        'id': uczen['id']})
