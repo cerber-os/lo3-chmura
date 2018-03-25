d22bfe7a46f9ee3c37fea4ce6dd8b099fe81bdfb
Lachcim <jajnick@hotmail.com>
2018-03-25 14:23:28 +0200

    Revamp admin panel, phase 1 of 2

--- chmura/templatetags/tags_extra.py
+++ chmura/templatetags/tags_extra.py
@@ -11,15 +11,6 @@ def get_item_or_zero(dictionary, key):
     return dictionary.get(key, 0)
 
 
-@register.filter
-def return_on_off(element, reverse=False):
-    txt = 'włączony' if element else 'wyłączony'
-    if bool(element) ^ bool(reverse):
-        return '<span style="color: green;">' + txt + '</span>'
-    else:
-        return '<span style="color: red; font-weight: bold;">' + txt + '</span>'
-
-
 @register.filter()
 def set_color(level):
     text_decoration = ''
@@ -33,11 +24,3 @@ def set_color(level):
     else:
         color = 'inherit'
     return '<span style="color: ' + color + ';' + text_decoration + '">' + level + '</span>'
-
-
-@register.filter()
-def return_never_on_empty(element):
-    if element == '':
-        return '<span style="color: red; font-weight: bold;">nigdy</span>'
-    else:
-        return element
